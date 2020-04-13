d2c97497767fb286d33d17d7e94a2a8466cfccda
wuwei <990030750@qq.com>
2020-04-13 14:14:15 +0800

    perfect

--- config/setupProxy.js
+++ config/setupProxy.js
@@ -1,6 +1,9 @@
 module.exports = {
   '/api': {
-    target: 'https://other-server.example.com',
-    secure: false
+    changeOrigin: true,
+    // target: 'http://10.255.50.48:20081',  // 开发环境
+    target: 'http://10.160.1.174:8080',  // 高钦本地环境
+    // secure: false,
+    pathRewrite: { '^/api' : '' }
   }
 }
--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Icon = ({ type, style = {} }) => {
   return (
-    <svg class="icon svg-icon" style={style} width={style.fontSize} height={style.fontSize} aria-hidden="true">
+    <svg className="icon svg-icon" style={style} width={style.fontSize} height={style.fontSize} aria-hidden="true">
       <use xlinkHref={`#icon${type}`}></use>
     </svg>
   )
